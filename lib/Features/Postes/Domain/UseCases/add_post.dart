d1b914c63b04bb83500167d564126a05f79d9a4e
mod-ibr <eng.modibr30@gmail.com>
2023-03-06 15:19:58 +0200

    naming updates

--- lib/Features/Postes/Domain/UseCases/add_post.dart
+++ lib/Features/Postes/Domain/UseCases/add_post.dart
@@ -3,10 +3,10 @@ import 'package:clean_architecture_demo_app/Features/Postes/Domain/Entites/post_
 import 'package:clean_architecture_demo_app/Features/Postes/Domain/Repositories/postes_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class AddPostuseCase {
+class AddPostUseCase {
   final PostesRepository postesRepository;
 
-  AddPostuseCase(this.postesRepository);
+  AddPostUseCase(this.postesRepository);
 
   Future<Either<Failure, Unit>> call(Post post) async {
     return await postesRepository.addPost(post);
